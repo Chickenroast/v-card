2f512936db2bd7759f7aa2aee5e5c118a3299e2d
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-10 16:18:14 +0100

    card props

--- v-card/src/App.tsx
+++ v-card/src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import CardProps from "./components/Vcard.tsx";
+import CardProps from "../src/components/vcard";
 
 function App() {
   return (
     <div>
-      <h1>My React App</h1>
-      <CardProps
-        title="Example Card"
-        content="This is the content of the card."
-      />
+      <div>
+        <CardProps
+          title="Julia Michel T."
+          statut="Student junior web developer"
+          school="at Becode"
+        />
+      </div>
     </div>
   );
 }
--- v-card/src/components/vcard.tsx
+++ v-card/src/components/vcard.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 interface CardProps {
   title: string;
-  content: string;
+  statut: string;
+  school: string;
 }
 
-const CardProps: React.FC<CardProps> = ({ title, content }) => {
+const CardProps: React.FC<CardProps> = ({ title, school, statut }) => {
   return (
-    <div className="card">
-      <h1>{title}</h1>
-      <p>{content}</p>
+    <div className="card-wrapper">
+      <div className="card">
+        <h1>{title}</h1>
+        <p>{statut}</p>
+        <p>{school}</p>
+      </div>
     </div>
   );
 };
