b3d3f31459d41e6d33f473a19f81c2a2772ef9b5
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-11 21:12:29 +0100

    simple card

--- v-card/src/components/vcard.tsx
+++ v-card/src/components/vcard.tsx
@@ -10,9 +10,9 @@ const CardProps: React.FC<CardProps> = ({ title, school, statut }) => {
   return (
     <div className="card-wrapper">
       <div className="card">
-        <h1>{title}</h1>
-        <p>{statut}</p>
-        <p>{school}</p>
+        <h1 className="h1">{title}</h1>
+        <p className="p">{statut}</p>
+        <p className="p">{school}</p>
       </div>
     </div>
   );
