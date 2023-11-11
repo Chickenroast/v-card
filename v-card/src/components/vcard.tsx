62585d6e78ae4f2634d0166807e9035d80a1b280
Julia Michel T. <juliamicheltex@gmail.com>
2023-11-11 22:49:40 +0100

    flipped animation

--- v-card/src/components/vcard.tsx
+++ v-card/src/components/vcard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import "../App.css"; // Import your CSS file
+import { motion, useAnimation } from "framer-motion";
 
 interface CardProps {
   title: string;
@@ -7,103 +8,35 @@ interface CardProps {
   school: string;
 }
 
-const VCard: React.FC<CardProps> = ({ title, school, statut }) => {
+const CardProps: React.FC<CardProps> = ({ title, school, statut }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const controls = useAnimation();
 
   const handleCardClick = () => {
+    controls.start({ rotateY: isFlipped ? 0 : 180 });
     setIsFlipped(!isFlipped);
   };
 
   return (
-    <CardContainer onClick={handleCardClick} isFlipped={isFlipped}>
-      <FrontFace>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className={`card-wrapper ${isFlipped ? "flipped" : ""}`}
+      onClick={handleCardClick}
+    >
+      <motion.div
+        className="box"
+        animate={controls}
+        initial={{ rotateY: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <h1 className="h1">{title}</h1>
         <p className="p">{statut}</p>
         <p className="p">{school}</p>
-        <WavyShape />
-      </FrontFace>
-      <BackFace>
-        {/* Content for the back face */}
-        <p>This is the back face</p>
-      </BackFace>
-    </CardContainer>
+      </motion.div>
+    </motion.div>
   );
 };
 
-// Define the animation
-const cardFlipAnimation = keyframes`
-  0% {
-    transform: rotateY(0deg);
-  }
-  100% {
-    transform: rotateY(180deg);
-  }
-`;
-
-// Style the component with the animation
-const CardContainer = styled.div<{ isFlipped: boolean }>`
-  position: relative;
-  width: 200px;
-  height: 300px;
-  perspective: 1000px;
-  transform-style: preserve-3d;
-  transition: transform 0.6s;
-  transform: ${(props) =>
-    props.isFlipped ? "rotateY(180deg)" : "rotateY(0deg)"};
-  cursor: pointer;
-  animation: ${cardFlipAnimation} 2s infinite; // Add this line to use the animation
-`;
-
-const Face = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-`;
-
-const FrontFace = styled(Face)`
-  background-color: white;
-  padding: 20px;
-  border-radius: 10px;
-`;
-
-const BackFace = styled(Face)`
-  background-color: lightblue; /* Change the color for the back face */
-  padding: 20px;
-  border-radius: 10px;
-  transform: rotateY(180deg);
-`;
-
-// Create a wave-like pattern for the liquid effect
-const WavyShape = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 20px;
-  background: linear-gradient(to right, #ffffff 25%, #f0f0f0 50%, #ffffff 75%);
-  &:before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 20px;
-    background: linear-gradient(
-      to right,
-      #ffffff 25%,
-      #f0f0f0 50%,
-      #ffffff 75%
-    );
-    transform: skewX(-20deg);
-    animation: waveAnimation 2s infinite linear;
-  }
-  @keyframes waveAnimation {
-    0% {
-      transform: translateX(0);
-    }
-    100% {
-      transform: translateX(-100%);
-    }
-  }
-`;
-
-export default VCard;
+export default CardProps;
